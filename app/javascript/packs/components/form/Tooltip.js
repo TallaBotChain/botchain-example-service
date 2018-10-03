5ca76a09866209347527a3409aa9759710556a34
Sergey Tarakanov <zolotara@gmail.com>
2018-10-03 21:21:50 +0300

    Custom tooltip styles

--- app/javascript/packs/components/form/Tooltip.js
+++ app/javascript/packs/components/form/Tooltip.js
@@ -3,7 +3,7 @@ import React, { Component }  from 'react';
 class Tooltip extends Component {
   render() {
     return (
-      <div className='tooltip'>
+      <div className='tooltip1'>
         <div>
           <span></span>
           <div className='popup'>
