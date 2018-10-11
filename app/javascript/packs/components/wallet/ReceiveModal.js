6eb76da3f9f95490305473ee64d7f5c1e9927b39
Sergey Tarakanov <zolotara@gmail.com>
2018-10-11 11:52:55 +0300

    Fixed "Copy address" link

--- app/javascript/packs/components/wallet/ReceiveModal.js
+++ app/javascript/packs/components/wallet/ReceiveModal.js
@@ -1,14 +1,9 @@
 import React, { Component } from 'react';
 import { Modal, Button, Alert, Well } from 'react-bootstrap';
 import QRCode from 'qrcode.react';
-// import {clipboard} from 'electron'
+import Clipboard from 'react-clipboard.js';
 
 export default class ReceiveModal extends Component {
-
-  copyAddressClick = (e) => {
-    e.preventDefault();
-    // clipboard.writeText(this.props.address)
-  }
 
   render() {
     return (
@@ -21,10 +16,10 @@ export default class ReceiveModal extends Component {
             <QRCode value={this.props.address} renderAs="svg" size={132} />
           </div>
           <h3 className="gray-text">Your {this.props.currency} Address</h3>
-          <h3 className={`${this.props.currency}`}>{this.props.address}</h3>
+          <h3 className={`${this.props.currency}`} id="my_address">{this.props.address}</h3>
         </Modal.Body>
         <Modal.Footer className="text-center">
-          <a href="#" onClick={this.copyAddressClick}><small className="state-text">Copy address</small></a>
+          <Clipboard className="btn btn-link state-text" data-clipboard-target="#my_address">Copy address</Clipboard>
         </Modal.Footer>
       </Modal>
     );
