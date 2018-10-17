c14a5a82c139bffe6bce9c3a13f61779feef9aaa
Sergey Tarakanov <zolotara@gmail.com>
2018-10-17 12:11:11 +0300

    Draft of new registration form

--- app/javascript/packs/components/developer/DeveloperForm.js
+++ app/javascript/packs/components/developer/DeveloperForm.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Field, reduxForm } from 'redux-form';
-import { required, length, url } from 'redux-form-validators'
+import {required, length, url, email} from 'redux-form-validators'
 import { MetadataValidator } from '../../validators/MetadataValidator';
 import { inputField, textareaField } from '../form/FormFields';
 import {connect} from 'react-redux'
@@ -27,7 +27,50 @@ class DeveloperForm extends Component {
           component={inputField} label="ETH Address" placeholder="0x000..."
           validate={[ required()]}
         />
-        <Field name="metadata_url" type="url"
+        <Field name="name" type="text"
+          component={inputField} label="Name" placeholder="Name"
+          validate={[required(), length({min: 8})]}
+        />
+        <Field name="organization" type="text"
+          component={inputField} label="Organization" placeholder="Organization"
+          validate={[ required()]}
+        />
+        <Field name="street_1" type="text"
+          component={inputField} label="Street 1" placeholder="Street 1"
+          validate={[ required()]}
+        />
+        <Field name="street_2" type="text"
+          component={inputField} label="Street 2" placeholder="Street 2"
+        />
+        <Field name="city" type="text"
+          component={inputField} label="City" placeholder="City"
+          validate={[ required()]}
+        />
+      <Field name="state/province" type="text"
+          component={inputField} label="State/Province" placeholder="State/Province"
+          validate={[ required()]}
+        />
+        <Field name="postal_code" type="text"
+          component={inputField} label="Postal Code" placeholder="Postal Code"
+          validate={[ required()]}
+        />
+        <Field name="country" type="text"
+          component={inputField} label="Country" placeholder="Country"
+          validate={[ required()]}
+        />
+        <Field name="phone" type="text"
+          component={inputField} label="Phone" placeholder="Phone"
+          validate={[ required()]}
+        />
+        <Field name="email" type="text"
+          component={inputField} label="Email" placeholder="Email"
+          validate={[required(), email()]}
+        />
+        <Field name="url" type="text"
+          component={inputField} label="Organization URL" placeholder="Organization URL"
+          validate={[ required(), length({ max: 132 }), url() ]}
+        />
+        {/* <Field name="metadata_url" type="url"
           component={inputField} label="Developer Metadata URL" placeholder="Developer Metadata URL"
           validate={[ required(), length({ max: 132 }), url() ]}
           appendComponent={<MetadataTooltip />}
@@ -35,7 +78,7 @@ class DeveloperForm extends Component {
         <Field name="metadata"
           component={textareaField} label="Metadata" placeholder="Will be autoloaded from url above" readOnly={true}
           validate={[required(), validateMetadata]}
-        />
+        /> */}
         <Button bsClass="btn orange-button cta-button" type="submit">
           REGISTER
         </Button>
