99de3c5b80898ef2e121501063bced9d447a0170
Sergey Tarakanov <zolotara@gmail.com>
2018-11-02 16:50:27 +0300

    Fixed #18

--- app/javascript/packs/containers/Developer.js
+++ app/javascript/packs/containers/Developer.js
@@ -77,7 +77,6 @@ class DeveloperPage extends Component {
             <p className="botcoin-green">
               <strong>Botchain Developer</strong>
             </p>
-            <p className='alert-info'>Note : You have to be pre-approved to successfully complete the registration. Please <a href="https://botchain.talla.com/developers">click here</a> to request approval. Read more about the Developer Registration Process <a href="/faq#developer_registration" target="_blank">here.</a></p>
             <Errors errors={this.props.developer.errors} />
             <DeveloperForm onSubmit={this.submit} submitDisabled={this.props.developer.ipfsInProgress}/>
             <PaymentModal 
