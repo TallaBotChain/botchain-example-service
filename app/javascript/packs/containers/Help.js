64b45f18b62b5111f6e1a321b9171d6a0aff5d14
Server Izetov <izetov.server@gmail.com>
2018-11-07 10:17:40 +0300

    updated help content

--- app/javascript/packs/containers/Help.js
+++ app/javascript/packs/containers/Help.js
@@ -1,17 +1,14 @@
 import React, { Component } from 'react';
-import {connect} from 'react-redux'
 
 class HelpPage extends Component {
 
-
   render() {
-
     return (
       <div className="white-container">
         <div className='inner-container help'>
-          <h1>Help</h1>
+          <h1>Developer Registration FAQ/Help</h1>
           <h3 className="green-text" id="how_to_register">How to Register</h3>
-          <p>The BotChain Registration service provides an simple way for developers to register on BotChain.</p>
+          <p>The BotChain Registration service provides a simple way for developers to register on BotChain.</p>
           
           <h3 className="green-text" id="register_steps">What are the steps to Register</h3>
           <p>
@@ -19,15 +16,15 @@ class HelpPage extends Component {
             <ul>
               <li>Use BotChain Registration Service to enter developer information</li>
               <li>Developer information is then submitted to the BotChain curators for Approval</li>
-              <li>BotChain Curators review developer information and choose to appove or reject the developer registration</li>
+              <li>BotChain Curators review developer information and choose to approve or reject the developer registration</li>
             </ul>
           </p>
 
           <h3 className="green-text" id="register_process">How does the Registration process work</h3>
           <p>
-            Once the developer enter their information. There are two things that happen in the background.<br/>
+            Once the developer enters their information. There are two things that happen in the background.<br/>
             A JSON file with metadata will be constructed and sent to IPFS.
-            BotChain smartcontracts running on the Ethereum blockchain will be invoked with the developer ETH address and the JSON file stored on IPFS
+            BotChain smart contracts running on the Ethereum blockchain will be invoked with the developer ETH address and the JSON file stored on IPFS
             The ETH address along with JSON metadata will serve as the developer identity information and will be submitted to the BotChain Curation Council for Approval.
           </p>
 
@@ -46,9 +43,14 @@ class HelpPage extends Component {
             BotChain runs on the Ethereum BlockChain. ETH is the currency of the Ethereum blockchain.
           </p>
 
-          <h3 className="green-text" id="what_is_gas_cost">What is Gas Cost?</h3>
+          <h3 className="green-text" id="what_is_gas">What is Gas?</h3>
           <p>
             Operations in Ethereum blockchain costs Gas which is paid in ETH. The cost per transaction is calculated based on the current unit gas price and the volume of gas used.
+          </p>
+
+          <h3 className="green-text" id="where_get_eth">Where do I get ETH to pay for gas?</h3>
+          <p>
+            For the test network, you can get ETH here:  <a href="https://github.com/kovan-testnet/faucet" target="_blank" rel="noopener noreferrer">https://github.com/kovan-testnet/faucet</a>
           </p>
           <br/>
           <br/>
@@ -75,7 +77,10 @@ class HelpPage extends Component {
               <a href="#what_is_eth">What is ETH?</a>
             </li>
             <li>
-              <a href="#what_is_gas_cost">What is Gas Cost?</a>
+              <a href="#what_is_gas">What is Gas?</a>
+            </li>
+            <li>
+              <a href="#where_get_eth">Where do I get ETH to pay for gas?</a>
             </li>
           </ul>
         </div>
