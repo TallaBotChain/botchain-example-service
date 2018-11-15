3e8c5bd19d5fc501629501c6c5ab51f628c6e660
Henry Wagner <henry@talla.com>
2018-11-15 11:32:50 -0500

    add force_ssl for all app requests. Defense against MITM.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
   include Clearance::Controller
-
+  force_ssl :except => [:health], :if => lambda { !Rails.env.development? && !Rails.env.test? }
   skip_before_action :verify_authenticity_token
 end
