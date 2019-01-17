25c4b4aeb6792505b3c2b16c73fae73a568fa8e5
Server Izetov <izetov.server@gmail.com>
2019-01-17 15:27:10 +0300

    added styles for NetworkSwitcher

--- app/javascript/components/shared/NetworkSwitcher.js
+++ app/javascript/components/shared/NetworkSwitcher.js
@@ -3,20 +3,33 @@ import { NavDropdown, MenuItem } from 'react-bootstrap';
 
 class NetworkSwitcher extends React.Component {
 
+  constructor(props) {
+    super(props);
+    this.state = { switchingNetwork: false };
+  }
+
   handleSelect(eventKey, event) {
     event.preventDefault();
+    this.setState({switchingNetwork: true})
     window.keyTools.currentNetwork = eventKey;
     location.reload(); 
   }
 
+  renderLoader(){
+    return(
+      <NavDropdown eventKey={7} title="Switching Network" id="networks-dropdown" className="network-switcher"></NavDropdown>
+    )
+  }
+
   render() {
+    if (this.state.switchingNetwork) return this.renderLoader();
     const current_network = window.keyTools.currentNetwork;
     const networks = window.app_config.eth_networks.networks;
 
     return (
-      <NavDropdown eventKey={7} title={networks[current_network].network_name} id="networks-dropdown" onSelect={(k,e) => this.handleSelect(k,e)}>
+      <NavDropdown eventKey={7} title={networks[current_network].network_name} id="networks-dropdown" className={`network-switcher ${current_network}`} onSelect={(k,e) => this.handleSelect(k,e)}>
         {Object.keys(networks).map((key) => {
-          return (<MenuItem key={networks[key].network_id} eventKey={key}> { networks[key].network_name}</MenuItem>)
+          return (<MenuItem key={networks[key].network_id} eventKey={key} className={key}>{networks[key].network_name}</MenuItem>)
         })}
       </NavDropdown>
     )
